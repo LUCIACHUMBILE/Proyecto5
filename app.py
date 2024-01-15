f9e5756149e71a673ba6423cdb20b185eb49463f
LUCIACHUMBILE <lucia.chumbile@gmail.com>
2024-01-15 17:43:28 -0500

    Cambio de ruta

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import streamlit as st
 import plotly_express as px
+
 data = pd.read_csv(
-    "C:/Users/LUCIA/OneDrive/ANÁLISIS DE DATOS/VISUAL BOX/Proyecto7/vehicles_us.csv")
-st.header("Explorando Opciones: Resumen de Anuncios de Autos en Venta :red_car:",divider='rainbow')  # Título principal
+    "C:/Users/LUCIA/OneDrive/ANÁLISIS DE DATOS/Visual Studio Code/Proyecto7/vehicles_us.csv")
+st.header("Explorando Opciones: Resumen de Anuncios de Autos en Venta :red_car:",
+          divider='rainbow')  # Título principal
 
 hist_button = st.button('Construir histograma')  # crear un botón
 if hist_button:  # al hacer clic en el botón
@@ -20,4 +22,4 @@ build_histogram = st.checkbox("Construir gráfica de dispersión")
 if build_histogram:  # si la casilla de verificación está seleccionada
     st.write('Precio vrs Kilometraje')
     fig1 = px.scatter(data, x="odometer", y="price")
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig1, use_container_width=True)
