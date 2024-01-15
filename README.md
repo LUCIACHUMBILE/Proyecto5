c67801c25beb3728d4eeb2f56b0a0fc345a66b27
LUCIACHUMBILE <lucia.chumbile@gmail.com>
2024-01-15 16:26:12 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Proyecto7
 Conjunto de datos de anuncios de coches
+Muestra un histograma de la frecuencia de kilometraje de los coches, asi como un diagrama de dispersion de precio vrs kilometraje
--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import streamlit as st
 import plotly_express as px
 data = pd.read_csv(
     "C:/Users/LUCIA/OneDrive/ANÁLISIS DE DATOS/VISUAL BOX/Proyecto7/vehicles_us.csv")
-st.header('Explorando Opciones: Resumen de Anuncios de Autos en Venta :red_car:',
-          divider='rainbow')  # Título principal
+st.header("Explorando Opciones: Resumen de Anuncios de Autos en Venta :red_car:",divider='rainbow')  # Título principal
 
 hist_button = st.button('Construir histograma')  # crear un botón
 if hist_button:  # al hacer clic en el botón
@@ -21,4 +20,4 @@ build_histogram = st.checkbox("Construir gráfica de dispersión")
 if build_histogram:  # si la casilla de verificación está seleccionada
     st.write('Precio vrs Kilometraje')
     fig1 = px.scatter(data, x="odometer", y="price")
-    fig1.show()  # crear gráfico de dispersión
+    st.plotly_chart(fig, use_container_width=True)
